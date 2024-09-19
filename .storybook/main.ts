6ee9a70956ae85c02f979f8afec0d410e99e2d63
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-09-19 12:30:45 +0800

    feat: create separate vite config for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import viteConfig from '../vite.config.storybook';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -13,6 +15,8 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
-  viteFinal: undefined
+  viteFinal: async (config) => {
+    return mergeConfig(config, viteConfig);
+  },
 };
 export default config;
